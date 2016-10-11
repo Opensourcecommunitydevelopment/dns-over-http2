9c9d4ad6337172885071a233bb148b6d4243bd6c
Opensourcecommunitydevelopment <fresvgkj@guerrillamail.com>
2016-10-11 16:04:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,144 @@
-const dnsd = require('dnsd');
+process.setMaxListeners(0);
+const dnsd = require('./dnsd/named');
 const spdy = require('spdy');
+const randomstring = require("randomstring");
+const forwardurl ='https://dns.google.com:443/resolve';//https://developers.google.com/speed/public-dns/docs/dns-over-https
+const url = require('url'); 
+const resolver = url.parse(forwardurl);
+
 const request = require('request').defaults({
   agent: spdy.createAgent({
-    host: 'dns.google.com',
-    port: 443
+    host: resolver.hostname,
+    port: resolver.port
+  }).once('error', (err) => {
+    console.error('agent error: %s', err);
   }),
   json: true
 });
-const subnet = process.argv[2];
-const RRTypes = require('./RRTypes').IntToString;
-const RRTypesByString = require('./RRTypes').StringToInt;
 
-const dns = dnsd.createServer((req, res) => {
-  console.log(
-    '%s:%s/%s %j',
-    req.connection.remoteAddress, req.connection.remotePort,
-    req.connection.type, req
-  );
+const subnet = process.argv[4];
+const Constants = require('./dnsd/constants');
+const SupportTypes = ['A', 'MX', 'CNAME', 'TXT', 'PTR', 'AAAA'];
+const ip6 = require('ip6')
+const server = dnsd.createServer((req, res) => {
   let question = req.question[0], hostname = question.name;
-  // TODO unsupported for now, due to dnsd's broken implementation.
-  if (question.type == 'AAAA') return res.end();
+  let timeStamp = `[${req.id}/${req.connection.type}] ${req.opcode} ${hostname} ${question.class} ${question.type}`;
+  console.time(timeStamp);
+  // TODO unsupported due to dnsd's broken implementation.
+  if (SupportTypes.indexOf(question.type) === -1) {
+    console.timeEnd(timeStamp);
+    return res.end();
+  } else{fallback=false;fallbackready=false;
+	  if(req.question[0].type=='A'){
+		  question.type='AAAA'; console.log('Testing AAAA for', hostname);
+		  let timeStamp6 = `[${req.id}/${req.connection.type}] ${req.opcode} ${hostname} ${question.class} ${question.type}`;
+		  console.time(timeStamp6);
+		  padding=  randomstring.generate({//API clients concerned about possible side-channel privacy attacks using the packet sizes of HTTPS GET requests can use this to make all requests exactly the same size by padding requests with random data. 
+			  length: 263-question.name.length-question.type.length,//maximum dnslength+NSEC3PARAM.length (longest possible Type now) minus current To make always equal query lenght url
+			  charset: 'alphanumeric'//safe but can be more extended chars-_ 
+			    });
+			    query={
+			      edns_client_subnet:   subnet,
+			      name: hostname,
+			      type: Constants.type_to_number(question.type),
+			      random_padding:   padding
+			    }   
+			    if( typeof subnet !== 'undefined' && subnet )query[0]=''; //allow approximate network Information if not specified
+		
+			  request({
+			    url: forwardurl,
+			    qs: query
+			  }, (err, response, output) => {//console.dir(output.Authority)      //res['recursion_available']=true;response['recursion_available']=true; // Always true for Google Public DNS      
+				  if(typeof output.Authority!=='undefined'){fallback=true;}
+				  	else if(typeof output.Answer!=='undefined'){ console.log('prefer ipv6');
+							  
+							  if (output && output.Answer && output.Question[0]['type']==28) {      
+						      res.answer = output.Answer.map(rec => {
+						        rec.ttl = rec.TTL;
+						        rec.type = Constants.type_to_label(rec.type);
+						      console.dir(rec) ;
+						       //cname
+						      if (rec.type== 'AAAA')					          	rec.data = ip6.normalize(rec.data); //fix dnsd/encode.js:132-133 As expects long IPVersion 6 format
+					          	
+						      
+						        return rec;
+						      });
+						    } else if (err) {
+						      console.error('request error %s', err);
+						    }
+						    console.timeEnd(timeStamp6);
+						    res.end();}
+						  });
+			  question.type='A'};  
+
+	  padding=  randomstring.generate({//API clients concerned about possible side-channel privacy attacks using the packet sizes of HTTPS GET requests can use this to make all requests exactly the same size by padding requests with random data. 
+		  length: 263-question.name.length-question.type.length,//maximum dnslength+NSEC3PARAM.length (longest possible Type now) minus current To make always equal query lenght url
+		  charset: 'alphanumeric'//safe but can be more extended chars-_ 
+		    });
+	  
+		  
+		  
+		    query={
+		      edns_client_subnet:   subnet,
+		      name: hostname,
+		      type: Constants.type_to_number(question.type),
+		      random_padding:   padding
+		    }   
+		    if( typeof subnet !== 'undefined' && subnet )query[0]=''; //allow approximate network Information if not specified
+		  
+	  
+
   request({
-    url: 'https://dns.google.com/resolve',
-    qs: {
-      edns_client_subnet: subnet,
-      name: hostname,
-      type: RRTypesByString[question.type]
-    }
-  }, (err, response, output) => {
-    if (output && output.Answer) {
+    url: forwardurl,
+    qs: query
+  }, (err, response, output) => {//console.dir(output)      //res['recursion_available']=true;response['recursion_available']=true; // Always true for Google Public DNS      
+	  
+//	  if (!output.Answer&& output.Question[0]['type']==28){//nxDomain No Records found TODO exclude cname
+//    	  console.log('nxDomain');
+ //     }
+	  
+	  if (output && output.Answer) {      
       res.answer = output.Answer.map(rec => {
         rec.ttl = rec.TTL;
-        rec.type = RRTypes[rec.type];
-        if (rec.type == 'MX') {
-          rec.data = rec.data.split(/\s+/);
+        
+        
+        
+        rec.type = Constants.type_to_label(rec.type);
+        console.dir(rec) ;
+        switch (rec.type) {
+          case 'MX':
+            rec.data = rec.data.split(/\s+/);
+            break;
+          case 'TXT':
+          case 'SPF':
+            rec.data = rec.data.slice(1, -1);
+            break;
+          case 'AAAA':
+          	rec.data = ip6.normalize(rec.data); //fix dnsd/encode.js:132-133 As expects long IPVersion 6 format
+          	break;
+          case 'A':
+          	fallbackready=true;
+          	break;
         }
-        delete rec.TTL;
-        return rec;
+        if(!fallbackready || fallback)return rec;
       });
     } else if (err) {
-      console.error(err);
+      console.error('request error %s', err);
     }
-    res.end();
-  }).on('error', err => {
-    console.error(err);
+    console.timeEnd(timeStamp);
+    if(!fallbackready || fallback)res.end();
   });
+	}
 });
 
-dns.on('error', err => {
-  console.error(err);
+server.once('error', err => {
+  console.error('dnsd error: %s', err);
 });
 
-dns.listen(process.argv[3] || 5353, process.argv[4] || '127.0.0.1');
+const devnull = require('dev-null');
+setInterval(() => {
+  let ping = forwardurl +'?name=' +resolver.hostname;
+  request(ping).pipe(devnull());
+}, 60 * 1000);
+
+server.listen(process.argv[2] || 6666, process.argv[3] || '127.0.0.1');

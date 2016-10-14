f67def7f0c1aa9827909dd0534429e4de72ff94c
Opensourcecommunitydevelopment <fresvgkj@guerrillamail.com>
2016-10-14 03:00:43 +0200

    Fix: IPv6Preference fallback + add Resolvercomment "Response from x.x.x.x"

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const Constants = require('./dnsd/constants');
 const ip6 = require('ip6');
 
 const subnet = process.argv[4];
-const SupportTypes = ['A', 'MX', 'CNAME', 'TXT', 'PTR', 'AAAA'];
+const SupportTypes = ['A', 'MX', 'CNAME', 'TXT', 'PTR', 'AAAA', 'NS'];
 
 const server = dnsd.createServer((req, res) => {
 	let question = req.question[0], 
@@ -37,11 +37,11 @@ const server = dnsd.createServer((req, res) => {
 	console.time(timeStamp);	
 	
 	// TODO unsupported due to dnsd's broken implementation.
-	if (SupportTypes.indexOf(question.type) === -1) {
-		console.timeEnd(timeStamp);
-		return res.end();
-	} else {
-		let fallback = false; let fallbackready = false;
+	//if (SupportTypes.indexOf(question.type) === -1) {
+	//	console.timeEnd(timeStamp);
+	//	return res.end();
+	//} else 
+	//{
 		if (req.question[0].type == 'A') {
 			question.type = 'AAAA';
 			let timeStamp6 = `[${req.id}/${req.connection.type}] Preference ${req.opcode} ${hostname} ${question.class} ${question.type}`;
@@ -70,11 +70,49 @@ const server = dnsd.createServer((req, res) => {
 				url: forwardUrl,
 				qs: query
 			}, (err, response, output) => {
-				if (typeof output.Authority !== 'undefined') { 
-					fallback = true; 
-					//fix A Query blocking
-					
-					}
+				if (typeof output.Authority !== 'undefined') {
+					// fix A Query blocking
+					//Reset Back to and for Original query Type fallback
+					question.type = 'A';
+					let padding = randomstring.generate({
+						// maximum dnslength+type.length (longest possible Type 5 digits)
+						// minus current To make always equal query lenght url
+						length: 257 - question.name.length ,
+						// safe but can be more extended chars-_
+						charset: 'alphanumeric'
+					})
+					let query = {
+							name: hostname,
+							type: 1,
+							random_padding: padding
+				    }
+
+				    if (subnet) {
+				    		query.edns_client_subnet = subnet;
+				    }
+
+					request({
+						url: forwardUrl,
+						qs: query
+					}, (err, response, output) => {
+						
+							if (output && output.Answer) {
+								res.answer = output.Answer.map(rec => {
+									rec.ttl = rec.TTL;
+									rec.type = Constants.type_to_label(rec.type);
+									return rec;
+								});
+							} else if (err) {
+								console.error('request error %s', err);
+							}
+							 if (typeof output.Comment !== 'undefined'){
+							    	//Resolvercomment "Response from x.x.x.x"	    		 
+							    	 process.stdout.write(output.Comment)
+							    }
+							console.timeEnd(timeStamp);
+							res.end();
+					});
+				}
 				else if (typeof output.Answer !== 'undefined') {
 					if (output && output.Answer && output.Question[0]['type'] === 28) {
 						res.answer = output.Answer.map(rec => {
@@ -90,74 +128,82 @@ const server = dnsd.createServer((req, res) => {
 					} else if (err) {
 						console.error('request error %s', err);
 					}
+					 if (typeof output.Comment !== 'undefined'){
+					    	//Resolvercomment "Response from x.x.x.x"	    		 
+					    	 process.stdout.write(output.Comment)
+					    }
 					console.timeEnd(timeStamp6);
 					res.end();
 				}
-			});
-			//Reset Back to and for Original query Type
-			question.type = 'A';
-		}  
-      
-	   // API clients concerned about possible side-channel privacy attacks
-	   // using the packet sizes of HTTPS GET requests can use this to make all
-	   // requests exactly the same size by padding requests with random data.
-	   let padding = randomstring.generate({
-	     // maximum dnslength+type.length (longest possible Type 5 digits)
-	     // minus current To make always equal query lenght url
-	     length: 258 - question.name.length - Constants.type_to_number(question.type),
-	     // safe but can be more extended chars-_
-	     charset: 'alphanumeric'
-	   });
+		});}
+		else {
+			//Original Query
+			   // API clients concerned about possible side-channel privacy attacks
+			   // using the packet sizes of HTTPS GET requests can use this to make all
+			   // requests exactly the same size by padding requests with random data.
+			   let padding = randomstring.generate({
+			     // maximum dnslength+type.length (longest possible Type 5 digits)
+			     // minus current To make always equal query lenght url
+			     length: 258 - question.name.length - Constants.type_to_number(question.type),
+			     // safe but can be more extended chars-_
+			     charset: 'alphanumeric'
+			   });
 
-	   let query = {
-	     name: hostname,
-	     type: Constants.type_to_number(question.type),
-	     random_padding: padding
-	   }
+			   let query = {
+			     name: hostname,
+			     type: Constants.type_to_number(question.type),
+			     random_padding: padding
+			   }
 
-	   if (subnet) {
-	     query.edns_client_subnet = subnet;
-	   }
+			   if (subnet) {
+			     query.edns_client_subnet = subnet;
+			   }
 
-	   request({
-	     url: forwardUrl,
-	     qs: query
-	   }, (err, response, output) => {
-	     if (output && output.Answer) {
-	       res.answer = output.Answer.map(rec => {
-	         rec.ttl = rec.TTL;
-	         rec.type = Constants.type_to_label(rec.type);
-	         switch (rec.type) {
-	           case 'MX':
-	             rec.data = rec.data.split(/\s+/);
-	             break;
-	           case 'TXT':
-	           case 'SPF':
-	             rec.data = rec.data.slice(1, -1);
-	             break;
-	           case 'AAAA':
-	             // dnsd is expecting long IPVersion 6 format
-	             rec.data = ip6.normalize(rec.data);
-	             break;
-		       case 'A':
-		         fallbackready=true;
-			     break;			                  
+			   request({
+			     url: forwardUrl,
+			     qs: query
+			   }, (err, response, output) => {
+			     if (output && output.Answer) {
+			    	 
+			       res.answer = output.Answer.map(rec => {
+			         rec.ttl = rec.TTL;
+			         rec.type = Constants.type_to_label(rec.type);
+			         switch (rec.type) {
+			           case 'MX':
+			             rec.data = rec.data.split(/\s+/);
+			             break;
+			           case 'TXT':
+			           case 'SPF':
+			             rec.data = rec.data.slice(1, -1);
+			             break;
+			           case 'AAAA':
+			             // dnsd is expecting long IPVersion 6 format
+			             rec.data = ip6.normalize(rec.data);
+			             break;
+						}
+
+						
+						return rec; 
+						
+					});
+				} else if (err) {
+					console.error('request error %s', err);
 				}
-
-				if (!fallbackready || fallback) { 
-					return rec; 
-					}
+			     if (typeof output.Comment !== 'undefined'){
+			    	//Resolvercomment "Response from x.x.x.x"	    		 
+			    	 process.stdout.write(output.Comment)
+			    }
+			    console.timeEnd(timeStamp);
+				res.end(); 
+			
 			});
-		} else if (err) {
-			console.error('request error %s', err);
+			
+			
 		}
-		console.timeEnd(timeStamp);
-		if (!fallbackready || fallback) { 
-			res.end(); 
-			}
-	});
-}
+			
+			
 });
+
 
 server.once('error', err => {
 console.error('dnsd error: %s', err);
